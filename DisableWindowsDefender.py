f299d2d4ed28003e39cbbc4479fbf70eb8209496
Always-Root <ibrahimkhan.aka.ghalib@gmail.com>
2025-06-27 22:21:37 -0700

    Update DisableWindowsDefender.py

--- DisableWindowsDefender.py
+++ DisableWindowsDefender.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, os, ctypes
 
 # disabling window defender
 def windead():
@@ -7,37 +7,50 @@ def windead():
                 r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviorMonitoring /t REG_DWORD /d 1 /f""",
                 r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 1 /f""",
                 r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 1 /f""",
-                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableIOAVProtection /t REG_DWORD /d 1 /f"""
-                ]
+                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableIOAVProtection /t REG_DWORD /d 1 /f""",
+                r"""shutdown -r /t 0"""]
 
-    # running for loop on the commands list and executing those commands
+
     for command in commands:
         try:
-            subprocess.call(command)
+                subprocess.call(command)
         except Exception as e:
-            pass
-        # when all the commands execution finished, and then we restart the system to take effect.
-        subprocess.call("shutdown -r /t 0")
+                print(e)
 
-windead()
-
-# compiling
-# pyinstaller -y -w -F --uac-admin --clean source_file.py
-# pyinstaller --noconfirm --noconsole --onefile --uac-admin --clean source_file.py
 
 # Restoring window defender to normal functioning
 def winlive():
-    commands = ["""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 0 /f""",
-                """reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 0 /f""",
-                """reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviorMonitoring /t REG_DWORD /d 0 /f""",
-                """reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 0 /f""",
-                """reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 0 /f""",
-                """reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableIOAVProtection /t REG_DWORD /d 0 /f"""
-                ]
+    commands = [r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 0 /f""",
+                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 0 /f""",
+                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableBehaviorMonitoring /t REG_DWORD /d 0 /f""",
+                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableScanOnRealtimeEnable /t REG_DWORD /d 0 /f""",
+                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableOnAccessProtection /t REG_DWORD /d 0 /f""",
+                r"""reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableIOAVProtection /t REG_DWORD /d 0 /f"""
+                r"""shutdown -r /t 0"""]
+
 
     for command in commands:
         try:
             subprocess.call(command)
         except Exception as e:
             print(e)
-        subprocess.call("shutdown -r /t 0")
+
+print("""\nDefender Killer
+Author: github.com/Always-Root
+Purpose: When malware researchers work on Windows,
+         Windows Defender often interferes significantly,
+         making it difficult to proceed. Disabling it becomes
+         a necessary step to continue research effectively.\n\n""")
+if ctypes.windll.shell32.IsUserAnAdmin():
+    response = input("1 => Disable Defender\n2 => Enable Defender\n3 => Exit\n# ")
+    if response == 1:
+        windead()
+    elif response == 2:
+        winlive()
+    else:
+        print("")
+else:
+    print("Please, run the script with admin privileges.")
+
+# compiling
+# pyinstaller --noconfirm --noconsole --onefile --uac-admin --clean source_file.py
